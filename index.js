53374c1513d3cf0064fc59faff6cea00f22338a8
robo-mop <realaryanpingle@gmail.com>
2024-05-11 16:15:02 +0530

    Simplify HTML structure + Improve media control UI

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ function setDeviation() {
     const timeSinceMs = (+new Date()) - probeLaunchTime;
     const minsSince = Math.floor(timeSinceMs / 1000 / 60);
     const secsSince = Math.floor((timeSinceMs / 1000) - minsSince * 60);
-    console.log(`%c${minsSince} MINUTES, ${secsSince} SECONDS AGO: Long-range probe successfully launched from the %cOrbital Probe Cannon%c.`, "color: cyan;", "color: orange; font-weight: bold;", "");
+    console.info(`%c${minsSince} MINUTES, ${secsSince} SECONDS AGO: Long-range probe successfully launched from the %cOrbital Probe Cannon%c.`, "color: cyan;", "color: orange; font-weight: bold;", "");
 }
 
 function startDeviationChecker() {
